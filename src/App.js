7edd899775ed9c2e37545651b9f1f213e206a6fa
RaphaelPl <160218@tuta.iol>
2020-05-19 16:37:16 +1000

    canvas created and exported

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React from 'react';
+import React from "react";
+import ReactRogue from "./ReactRogue";
 
 const app = () => {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+  return <div className="App"></div>;
+};
 
 export default App;

c06d16be412550989e3a2e67631af0372c9feafc
RaphaelPl <160218@tuta.iol>
2020-05-20 17:29:48 +1000

    input handler created to handle keycoded input using arrow keys

--- src/inputManager.js
+++ src/inputManager.js
@@ -1,10 +1,37 @@
-
 // creating a class to handle the player input via obsever pattern
 class inputManager {
-    observer = []
+  observer = [];
 
-    subscribe(fn){
-        this.observer.push(fn)
+  subscribe(fn) {
+    this.observer.push(fn);
+  }
+  unsubscribe(fn) {
+    this.observers = this.observers.filter((subscriber) => subscriber !== fn);
+  }
+  broadcast(action, data) {
+    this.observers.forEach((subscriber) => subscriber(action, data));
+  }
+  handleKeys = (e) => {
+    e.preventDefault();
+
+    //   create a managr for the keys being pressed to determine movement
+    switch (e.keyCode) {
+      //   left arrow, action is move and the data being passed in is a vector
+      case 37:
+        this.broadcast("move", { x: -1, y: 0 });
+        break;
+      //   up arrow, action move and data is vector
+      case 38:
+        this.broadcast("move", { x: 0, y: -1 });
+        break;
+      //   right arrow
+      case 39:
+        this.broadcast("move", { x: 1, y: 0 });
+        break;
+      // down arrow
+      case 38:
+        this.broadcast("move", { x: 0, y: 1 });
+        break;
     }
-
+  };
 }

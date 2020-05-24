2f41c0bd3b4515bcd0bab7a709aafb4ab07ede7d
RaphaelPl <160218@tuta.iol>
2020-05-24 21:34:12 +1000

    refactor player class now with entity

--- src/player.js
+++ src/player.js
@@ -1,18 +1,9 @@
-class Player {
-  constructor(x, y, size) {
-    this.x = x;
-    this.y = y;
-    this.size = size;
-  }
+import Entity from "./Entity";
+
+class Player extends Entity {
   move(dx, dy) {
     this.x += dx;
     this.y += dy;
-  }
-  draw(context) {
-    context.fillStyle = "#006600";
-    context.textBaseline = "hanging";
-    context.font = "16px";
-    context.fillText("@", this.x * this.size, this.y * this.size);
   }
   // copy player to allow check to see if player is currently in wall
   copyPlayer() {

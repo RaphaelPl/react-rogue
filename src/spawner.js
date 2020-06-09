46056871624dd6013a698a12c5f8e56f5a438d38
RaphaelPl <160218@tuta.iol>
2020-06-09 10:07:45 +1000

    spawner created

---
+++ src/spawner.js
@@ -0,0 +1,46 @@
+import Loot from "./Loot";
+
+const lootTable = [
+	{
+		name: "longsword",
+		colour: "darkgrey",
+		ascii: "/",
+		offset: { x: 6, y: 3 },
+    },
+    {
+		name: "mace",
+		colour: "darkgrey",
+		ascii: "/",
+		offset: { x: 6, y: 3 },
+    },
+    {
+		name: "bow",
+		colour: "darkgrey",
+		ascii: "}",
+		offset: { x: 6, y: 3 },
+    }
+    {
+		name: "potion",
+		colour: "red",
+		ascii: "!",
+		offset: { x: 6, y: 3 },
+    },
+    {
+		name: "Gold Coin",
+		colour: "yellow",
+		ascii: "$",
+		offset: { x: 3, y: 3 },
+    },
+    {
+		name: "Light Armour",
+		colour: "lightgrey",
+		ascii: "#",
+		offset: { x: 4, y: 3 },
+    },
+    {
+		name: "Leather Armour",
+		colour: "brown",
+		ascii: "#",
+		offset: { x: 4, y: 3 },
+    }
+];
